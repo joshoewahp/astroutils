4316f919a0bd9f8bb74d682062e76b2daece8c4a
Joshua Pritchard <joshua.pritchard@sydney.edu.au>
2023-10-09 12:59:59 +1100

    added force fitting functions

--- astroutils/source.py
+++ astroutils/source.py
@@ -5,10 +5,13 @@ import numpy as np
 import pandas as pd
 from astropy.coordinates import SkyCoord
 from astropy.nddata import Cutout2D
+from astropy.nddata.utils import NoOverlapError
 from astropy.table import Table
 from astropy.wcs import WCS
 from pathlib import Path
 from typing import Union, cast
+from forced_phot import ForcedPhot
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
 
 from astroutils.io import find_fields, get_image_data_header, get_survey, get_image_from_survey_params
 
@@ -102,13 +105,23 @@ class SelavyCatalogue:
 
         for field, sbid in zip(fields, sbids):
 
-            # First try locating catalogues in xml format, then try txt format
+            # First try locating catalogues in xml format
             files = list(selavypath.glob(f'*[._]{field}[AB.]*{sbid}.*components.xml'))
+
+            # then try txt format
             if len(files) == 0:
                 files = list(selavypath.glob(f'*[._]{field}[AB.]*{sbid}.*components.txt'))
 
+            # then try early RACS pattern with SBID first
+            if len(files) == 0:
+                files = list(selavypath.glob(f'*[._]{sbid}*{field}[AB.].*components.xml'))
+
+            # and finally early RACS text catalogues
+            if len(files) == 0:
+                files = list(selavypath.glob(f'*[._]{sbid}*{field}[AB.].*components.txt'))
+
             selavy_files.extend(files)
-            
+
         if len(selavy_files) == 0:
             raise FileNotFoundError(f"Could not locate selavy files at {selavypath} in either xml or txt format.")
 
@@ -263,6 +276,156 @@ def fractional_pol_error(
 
     return pol_error
 
+def force_measure_flux(
+        position: SkyCoord,
+        image: Path,
+        background: Path,
+        noise: Path,
+        size: u.Quantity,
+) -> pd.Series:
+
+    # Perform forced extraction
+    try:
+        FP = ForcedPhot(str(image), str(background), str(noise), verbose=True)
+        flux, flux_err, chisq, DOF, cluster_id = FP.measure(
+            position,
+            cluster_threshold=0,
+        )
+
+        is_limit = False
+        flux *= 1000
+        flux_err *= 1000
+        
+        # Ensure forced fit is above 3-sigma significance
+        rms_limit = measure_limit(position, noise, 30*u.arcsec)
+        rms = rms_limit / 3
+
+        if abs(flux) < rms_limit or np.isnan(flux):
+            flux = rms_limit
+            flux_err = np.nan
+            is_limit = True
+
+    except (FileNotFoundError, AttributeError, IndexError) as e:
+        logger.debug("Missing background/noise maps, measuring RMS from image.")
+
+        # If rms/background files missing, just use average survey RMS
+        try:
+            flux = measure_limit(position, image, size)
+            rms = flux / 3
+            flux_err = np.nan
+            is_limit = True
+        except NoOverlapError:
+            logger.debug(f"Image does not contain position <{position.ra:.2f},{position.dec:.2f}>")
+            return None, None
+
+    component = pd.Series({
+        'flux_peak': flux,
+        'flux_int': flux,
+        'flux_peak_err': flux_err,
+        'flux_int_err': flux_err,
+        'rms_image': rms,
+    })
+
+    return component, is_limit
+
+def measure_epoch_flux(
+        position: SkyCoord,
+        epoch: pd.Series,
+        size: u.Quantity,
+        fluxtype: str,
+        stokes: str,
+        tiletype: str,
+        name: str='source'
+):
+    # Get fields containing the source
+    ttype = 'COMBINED' if epoch.survey == 'racs-low' and stokes == 'v' else tiletype
+
+    fields = find_fields(position, epoch.survey, tiletype=ttype)
+
+    if len(fields) == 0:
+        return
+
+    if len(fields) > 1:
+        logger.debug(f"{epoch['name']} has more than one field containing {name}")
+
+    components = []
+    for _, field in fields.iterrows():
+
+        is_limit = False
+        is_forced = False
+
+        fieldname = field.field
+        _, header = get_image_from_survey_params(epoch, fieldname, stokes, ttype, load=False)
+
+        # First look for flux from selavy
+        try:
+            selavy = SelavyCatalogue.from_params(
+                epoch.survey,
+                stokes=stokes,
+                tiletype=ttype,
+                fields=fieldname,
+            )
+            component = selavy.nearest_component(position, radius=size)
+        except (FileNotFoundError, ValueError):
+            component = None
+
+        if component is None:
+            logger.debug(f"{epoch['name']} {fieldname} - no selavy component, performing forced fit")
+
+            image_file = list(Path(epoch[f'image_path_{stokes}_{ttype[0]}']).glob(f'*{fieldname}*.fits'))[0]
+            noise_filepattern = str(image_file).replace('IMAGES', 'RMSMAPS')
+            bkg_file = Path(noise_filepattern.replace('image.', 'meanMap.image.'))
+            rms_file = Path(noise_filepattern.replace('image.', 'noiseMap.image.'))
+
+            # Then force a measurement in the image and measure 3-sigma limit from RMS map
+            component, is_limit = force_measure_flux(
+                position,
+                image_file,
+                bkg_file,
+                rms_file,
+                size,
+            )
+            is_forced = True
+
+            # Neither selavy or forced fit possible. Move to next field.
+            if component is None:
+                continue
+
+        if not is_forced:
+            # Handle potential negative Stokes V fluxes
+            flux = component[f'flux_{fluxtype}'] * component.sign
+
+            # Calculate Condon flux error if component was found
+            bmaj, bmin = header['BMAJ']*u.deg, header['BMIN']*u.deg
+            flux_err = condon_flux_error(component, bmaj, bmin, fluxtype=fluxtype)
+            rms = component.rms_image
+        else:
+            flux = component[f'flux_{fluxtype}']
+            flux_err = component[f'flux_{fluxtype}_err']
+            rms = component.rms_image
+
+        try:
+            data = {
+                'source': name,
+                'epoch': epoch['name'],
+                'obsdate': header.get('DATE-OBS', ''),
+                'field': field.field,
+                'flux': flux,
+                'flux_err': flux_err,
+                'rms_image': rms,
+                'snr': abs(flux/rms),
+                'is_limit': is_limit,
+                'is_forced': is_forced,
+                'dist_field_centre': field.dist_field_centre,
+            }
+        except KeyError:
+            print(name, stokes, epoch['name'])
+            print(header.keys)
+            raise
+
+        components.append(data)
+
+    return pd.DataFrame(components)
 
 def measure_flux(
         position: SkyCoord,
@@ -275,67 +438,33 @@ def measure_flux(
 ) -> pd.DataFrame:
     """Measure flux or 3-sigma nondetection limit in multiple epochs at position."""
 
-    flux_list = []
-    for _, epoch in epochs.iterrows():
+    with ProcessPoolExecutor(max_workers=10) as executor:
+        processes = [
+            executor.submit(
+                measure_epoch_flux,
+                position,
+                epoch,
+                size,
+                fluxtype,
+                stokes,
+                tiletype,
+                name,
+            )
+            for _, epoch in epochs.iterrows()
+        ]
+    flux_list = [future.result() for future in processes if future.result() is not None]
 
-        # Get fields containing the source
-        fields = find_fields(position, epoch.survey)
-
-        if len(fields) == 0:
-            continue
-
-        if len(fields) > 1:
-            logger.warning(f"{epoch['name']} has more than one field containing {name}")
-
-        for _, field in fields.iterrows():
-
-            fieldname = field.field
-            _, header = get_image_from_survey_params(epoch, fieldname, stokes, tiletype, load=False)
-        
-            selavy = SelavyCatalogue.from_params(epoch['name'], fieldname, stokes)
-            component = selavy.nearest_component(position, radius=size)
-
-            if component is None:
-                # Measure 3-sigma limit from RMS map
-                image_file = list(Path(epoch[f'image_path_{stokes}_{tiletype[0]}']).glob(f'*{fieldname}*.fits'))[0]
-                rms_image = Path(
-                    str(image_file)
-                    .replace('IMAGES', 'RMSMAPS')
-                    .replace(f'.{stokes.upper()}.fits', f'.{stokes.upper()}_rms.fits')
-                )
-
-                flux = measure_limit(position, rms_image, 30*u.arcsec)
-                flux_err = np.nan
-                limit = True
-            else:
-                # Handle potential negative Stokes V fluxes
-                flux = component.flux_int * component.sign
-                
-                # Calculate Condon flux error if component was found
-                bmaj, bmin = header['BMAJ']*u.deg, header['BMIN']*u.deg
-                flux_err = condon_flux_error(component, bmaj, bmin, fluxtype=fluxtype)
-                limit = False
-
-            data = {
-                'source': name,
-                'epoch': epoch['name'],
-                'field': field.field,
-                'flux': flux,
-                'flux_err': flux_err,
-                'limit': limit,
-                'dist_centre': field.dist_field_centre,
-            }
-            flux_list.append(data)
-
-    flux_table = pd.DataFrame(flux_list)
-
-    return flux_table 
+    return pd.concat(flux_list) 
 
 
 def measure_limit(position: SkyCoord, image_path: Path, size: u.Quantity) -> pd.Series:
     """Calculate 3-sigma rms limit from at position in image."""
 
-    data, header = get_image_data_header(image_path)
+    try:
+        data, header = get_image_data_header(image_path)
+    except FileNotFoundError:
+        return
+
     wcs = WCS(header, naxis=2)
         
     try:
@@ -343,10 +472,10 @@ def measure_limit(position: SkyCoord, image_path: Path, size: u.Quantity) -> pd.
     except IndexError:
         cutout = Cutout2D(data, position, size, wcs=wcs)
 
-    if 'rms' in image_path.name:
-        return 3 * np.median(cutout.data)
+    if 'noise' in image_path.name:
+        return 3 * np.nanmedian(cutout.data)
     else:
-        return 3 * np.sqrt(np.mean(np.square(cutout.data)))
+        return 3 * np.sqrt(np.nanmean(np.square(cutout.data)))
 
 
 def measure_polarised_source(
@@ -493,9 +622,11 @@ def get_stokes_matches(
         field_coord = SkyCoord(ra=df.cr_ra_pix, dec=df.cr_dec_pix, unit='deg')
         c = SkyCoord(ra=df.I_ra_deg_cont, dec=df.I_dec_deg_cont, unit='deg')
         ra_offset, dec_offset = field_coord.spherical_offsets_to(c)
+        centre_dist = field_coord.separation(c)
 
         df['ra_offset'] = ra_offset.deg
         df['dec_offset'] = dec_offset.deg
+        df['field_centre_dist'] = centre_dist.deg
 
         logger.debug(f"{survey} - {field.field}")
         logger.debug(f"{len(cI)} I components, {len(cV)} V components")
@@ -504,3 +635,4 @@ def get_stokes_matches(
         components.append(df)
 
     return pd.concat(components)
+
